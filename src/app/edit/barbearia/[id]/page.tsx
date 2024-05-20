26ae0460dd8008e05f11a8dd5e42d88759e52b8b
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-05-20 12:58:46 -0300

    fix: modifying RouteParams for deployment

--- src/app/edit/barbearia/[id]/page.tsx
+++ src/app/edit/barbearia/[id]/page.tsx
@@ -1,19 +1,18 @@
-"use client"
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { EditForm } from "../../../edit/barbearia/[id]/EditForm";
 import { getById } from "@/app/actions/barbearia/get-by-id";
 
+interface RouteParams {
+  id: number;
+}
 
-export default async function EditarBarbearia({params}: Params) {
-  const { id } = params
+export default async function EditarBarbearia({ params }: { params: RouteParams }) {
+  const { id } = params;
 
-  const barbearia: Barbearia = await getById(id)
+  const barbearia: Barbearia = await getById(id);
 
   return (
     <main className="flex min-h-screen flex-col items-center ">
-
       <EditForm {...barbearia} />
-
     </main>
   );
 }
--- src/app/home/barbearia/[id]/page.tsx
+++ src/app/home/barbearia/[id]/page.tsx
@@ -1,10 +1,12 @@
 
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { Detalhar } from "./Detalhar";
 import { getById } from "@/app/actions/barbearia/get-by-id";
 
+interface RouteParams {
+  id: number;
+}
 
-export default async function DetalharBarbearia({params}: Params) {
+export default async function DetalharBarbearia({ params }: { params: RouteParams }) {
   const { id } = params
 
   const barbearia: Barbearia = await getById(id)
